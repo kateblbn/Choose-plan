532fccbd013c4c6c17f382941c595ced6d8a67be
kateblbn <125374342+kateblbn@users.noreply.github.com>
2023-06-10 08:38:28 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,6 @@
-const checkOrMark = document.querySelector('.checkOrMark');
-const button = document.querySelectorAll('.button');
+const plans = document.querySelector('.plans');
 
-button.forEach( btn => {
-    btn.addEventListener('click', function buttonAll() {
-        alert('Thank You for Your order!');
-
-    })
-});
-
-let plan = [ 
+let plan = [
     {
         support: 'Unlimited',
         space: 5,
@@ -16,7 +8,7 @@ let plan = [
         email: false,
         download: false
     },
-    {
+     {
         support: 'Unlimited',
         space: 10,
         user: 5,
@@ -31,14 +23,35 @@ let plan = [
         download: true
     }
 ];
+let mark = `<i class="fa-solid fa-xmark"></i>`;
+let check = `<i class="fa-solid fa-check"></i>`;
+let basic = '29$',
+    standart = '69$',
+    premium = '99$';
+
 for (let i = 0; i < plan.length; i++) {
-    const elem = plan[i];
-     console.log(elem.download);
-    let pic = (elem.download == false )? 'fa-solid fa-xmark' : 'fa-solid fa-check';
-console.log(pic);
-let test = `
-<div class="${pic}"></div>
-`
-// console.log(test);
-checkOrMark.innerHTML = test;
-}
+    let elem = plan[i];
+    let addSign = elem.download || elem.email ? check : mark;
+    let symbol = `
+    <div class="price__wrap">
+        <div class="price__plan">
+            <div class="price__plan-title">basic plan</div>
+            <div class="price__plan-price">${basic}</div>
+            <div class="price__plan-month">per month</div>
+        </div>
+        <div class="price__wrap-info">
+            <div class="info">${check} ${plan[i].support} support</div>
+            <div class="info">${check} ${plan[i].space}gb Server Space</div>
+            <div class="info">${check} ${plan[i].user} Users per Project</div>
+            <div class="info">${addSign} Email Integration</div>
+            <div class="info">${addSign} Unlimited Download</div>
+        </div>
+            <button class="button">choose plan</button>
+        </div>
+    </div>
+    `
+    let changes = i == 0 ? basic : 
+    i == 1 ? standart : 
+    i == 2 ? premium : 'error';
+    plans.innerHTML += symbol;
+};
