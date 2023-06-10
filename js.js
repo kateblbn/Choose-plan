c87378650f58ddabe97a95bdfb0a50d62b6769dd
kateblbn <125374342+kateblbn@users.noreply.github.com>
2023-06-10 12:04:14 +0200

    Update js.js

--- js.js
+++ js.js
@@ -31,12 +31,25 @@ let basic = '29$',
 
 for (let i = 0; i < plan.length; i++) {
     let elem = plan[i];
-    let addSign = elem.download || elem.email ? check : mark;
-    let symbol = `
-    <div class="price__wrap">
-        <div class="price__plan">
+    let addSign =  elem.email ? check : mark;
+    let addSignDown = elem.download ? check : mark;
+
+    let changes = i == 0 ? basic : 
+    i == 1 ? standart : 
+    i == 2 ? premium : 'error';
+
+
+    let wrapColor = i == 1 ? "blue" :
+    i == 2 ? "yellow" : "error";
+    // elem.classList.add(wrapColor);
+    console.log(wrapColor);
+
+
+    let wrapping = `
+    <div class="price__wrap ">
+        <div class="price__plan ">  
             <div class="price__plan-title">basic plan</div>
-            <div class="price__plan-price">${basic}</div>
+            <div class="price__plan-price">${changes}</div>
             <div class="price__plan-month">per month</div>
         </div>
         <div class="price__wrap-info">
@@ -44,14 +57,11 @@ for (let i = 0; i < plan.length; i++) {
             <div class="info">${check} ${plan[i].space}gb Server Space</div>
             <div class="info">${check} ${plan[i].user} Users per Project</div>
             <div class="info">${addSign} Email Integration</div>
-            <div class="info">${addSign} Unlimited Download</div>
+            <div class="info">${addSignDown} Unlimited Download</div>
         </div>
             <button class="button">choose plan</button>
         </div>
     </div>
     `
-    let changes = i == 0 ? basic : 
-    i == 1 ? standart : 
-    i == 2 ? premium : 'error';
-    plans.innerHTML += symbol;
+    plans.innerHTML += wrapping;
 };
